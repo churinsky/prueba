60b19a3c12db91fcb2299fe47461f7dba0209e26
jose de jesus mendoza <jjesus.mendoza.mendoza@gmail.com>
2025-04-07 14:19:36 -0600

    timer.h y timer.c terminados

--- presostato_digital/src/timer.h
+++ presostato_digital/src/timer.h
@@ -1,4 +1,13 @@
 #ifndef TIMER_H
 #define TIMER_H
+#include <avr/io.h>
+
+typedef struct
+{
+    volatile uint32_t contador_milli;
+} Timer;
+
+void timer_init(Timer *t);
+uint32_t millis(void);
 
 #endif
